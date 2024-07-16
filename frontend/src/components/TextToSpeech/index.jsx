ae9000869804984a899a9632f7620bdfd5a2fee4
Zahid Hasan <zahid.hasan@infolytx.com>
2024-07-16 17:26:32 +0600

    remove commented out code

--- frontend/src/components/TextToSpeech/index.jsx
+++ frontend/src/components/TextToSpeech/index.jsx
@@ -1,70 +1,3 @@
-// import React, { useState, useEffect } from "react";
-// import { MdOutlineAdjust, MdVolumeUp } from "react-icons/md";
-
-// const pitch = 1;
-// const rate = 1;
-// const volume = 1;
-
-// const TextToSpeech = ({ text }) => {
-//   const [isPaused, setIsPaused] = useState(false);
-//   const [utterance, setUtterance] = useState(null);
-//   const [voice, setVoice] = useState(null);
-
-//   useEffect(() => {
-//     const synth = window.speechSynthesis;
-//     const u = new SpeechSynthesisUtterance(text);
-//     setUtterance(u);
-
-//     synth.addEventListener("voiceschanged", () => {
-//       const voices = synth.getVoices();
-//       setVoice(voices[0]);
-//     });
-
-//     return () => {
-//       synth.cancel();
-//       synth.removeEventListener("voiceschanged", () => {
-//         setVoice(null);
-//       });
-//     };
-//   }, [text]);
-
-//   const handlePlay = () => {
-//     const synth = window.speechSynthesis;
-
-//     if (isPaused) {
-//       synth.resume();
-//     } else {
-//       utterance.voice = voice;
-//       utterance.pitch = pitch;
-//       utterance.rate = rate;
-//       utterance.volume = volume;
-//       synth.speak(utterance);
-//     }
-
-//     setIsPaused(false);
-//   };
-
-//   const handleStop = () => {
-//     const synth = window.speechSynthesis;
-//     setIsPaused(false);
-//     synth.cancel();
-//   };
-
-//   return (
-//     <div>
-
-//       <div
-//         className="tooltip tooltip-right hover:cursor-pointer"
-//         data-tip={isPaused ? "Read Aloud" : "Stop"}
-//         onClick={isPaused ? handlePlay : handleStop}
-//       >
-//         {isPaused ? <MdVolumeUp /> : <MdOutlineAdjust />}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default TextToSpeech;
 
 import React, { useState, useEffect } from "react";
 import { MdOutlineAdjust, MdVolumeUp } from "react-icons/md";
@@ -89,7 +22,7 @@ const TextToSpeech = ({ text }) => {
     };
 
     synth.addEventListener("voiceschanged", setVoices);
-    setVoices(); // Call immediately in case voices are already available
+    setVoices();
 
     return () => {
       synth.cancel();
